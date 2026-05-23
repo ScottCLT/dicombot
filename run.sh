9ca774488dbf6fa0738adafa33c72fe6121f7987
ScottCLT <48764334+ScottCLT@users.noreply.github.com>
2026-05-23 12:30:10 -0400

    Update Docker run command with new environment variables

--- run.sh
+++ run.sh
@@ -1,4 +1,4 @@
 #!/bin/bash
 docker stop dicombot
 docker rm dicombot
-docker run --name dicombot --env IPADDRESS="10.0.3.214" --env SERVERPORT="11112" --env CALLING="DICOMBOT" --env CALLED="DCM4CHEE" scottclt/dicombot
+docker run --name=dicombot --env AETITLE="DICOMBOT" --env STORAGEIP="10.1.1.100" --env STORAGEPORT="11112" --env STORAGEAE="DCM4CHEE" --env RETRIEVEIP="10.1.1.100" --env RETRIEVEPORT="11112" --env RETRIEVEAE="DCM4CHEE" --env DICOMFILES=1 -v /path/to/mount/folder:/var/dcm/mount scottclt/dicombot
